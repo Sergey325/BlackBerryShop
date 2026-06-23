ef43932736af66e05eeff31bf54f750bda67d3df
Sergey325 <sabserj2017@gmail.com>
2026-06-23 21:22:08 +0200

    ADDED base script for fb-pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import Container from "@/app/components/reusable/Container";
 import CartModal from "@/app/components/modals/CartModal";
 import Footer from "@/app/components/Footer";
 import SizesModal from "@/app/components/modals/SizesModal";
+import Script from "next/script";
 
 
 const montserrat = Montserrat({
@@ -42,6 +43,22 @@ export default function RootLayout({
                     </main>
                 </Container>
                 <Footer/>
+                <Script id="facebook-pixel" strategy="afterInteractive">
+                {`
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}
+                    (window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+        
+                    fbq('init', '1013434618078956');
+                    fbq('track', 'PageView');
+                `}
+                </Script>
             </body>
         </html>
     );
