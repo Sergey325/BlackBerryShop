f03ee1759ef4ecc234985eba58c83d69c84da752
Sergey325 <sabserj2017@gmail.com>
2026-06-24 20:32:41 +0200

    FIXED for delayed answer from webhook

--- src/app/components/reusable/Loader.tsx
+++ src/app/components/reusable/Loader.tsx
@@ -2,9 +2,16 @@
 
 import { PuffLoader } from "react-spinners";
 
-const Loader = () => {
+type Props = {
+    isFullScreen?: boolean;
+}
+
+const Loader = ({isFullScreen = true}: Props) => {
     return (
-        <div className="h-[70vh] flex flex-col justify-center items-center">
+        <div
+            style={{height: isFullScreen ? "70vh" : "30vh"}}
+            className="h-[70vh] flex flex-col justify-center items-center"
+        >
             <PuffLoader size={150}  color="#823D9A"/>
         </div>
     );
