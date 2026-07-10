0f1b33f2fe250f7085a35d261148c5ad6d251975
Sergey325 <sabserj2017@gmail.com>
2026-07-10 16:09:33 +0200

    ADDED Sentry integration

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a516dac973db83bae8b42d8fe0510ddf@o4505884618391552.ingest.us.sentry.io/4511695115386880",
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
